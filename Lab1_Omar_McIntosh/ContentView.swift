7591e14a6534e48049ca32440a25979de3a580de
Omar <omar.j.black@gmail.com>
2026-02-28 16:18:51 -0500

    Initial Commit

---
+++ Lab1_Omar_McIntosh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Omar_McIntosh
+//
+//  Created by Omar McIntosh on 2026-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Omar_McIntosh/Lab1_Omar_McIntoshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Omar_McIntoshApp.swift
+//  Lab1_Omar_McIntosh
+//
+//  Created by Omar McIntosh on 2026-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Omar_McIntoshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
